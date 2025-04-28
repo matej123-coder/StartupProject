6bde8068ff4e150b33fbfacbdc9e96a1e2497b62
MatejMinoski <mmatej.minoski@gmail.com>
2025-04-28 12:22:55 +0200

    Implementing search functionality

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -3,13 +3,6 @@ import SearchFormReset from "./SearchFormReset";
 import {Search} from "lucide-react";
 
 const SearchForm=({query} : {query?:string})=>{
-
-  const reset=()=>{
-    const form = document.querySelector(".search-form") as HTMLFormElement;
-    if(form){
-      form.reset();
-    }
-  }
   return(
       <Form action={"/"} scroll={false} className="search-form">
           <input name={"query"}
