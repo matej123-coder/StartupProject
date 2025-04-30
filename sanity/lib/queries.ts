c6fe26fa2e5b2a60d346ad8170bead2ce992b7b8
MatejMinoski <mmatej.minoski@gmail.com>
2025-04-30 10:33:05 +0200

    Adding final feature

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -78,3 +78,27 @@ export const GET_ALL_STARTUPS_BY_USER_ID= defineQuery(`
     image,
     pitch
 }`)
+export const PLAYLIST_BY_SLUG_QUERY =
+    defineQuery(`*[_type == "playlist" && slug.current == $slug][0]{
+  _id,
+  title,
+  slug,
+  select[]->{
+    _id,
+    _createdAt,
+    title,
+    slug,
+    author->{
+      _id,
+      name,
+      slug,
+      image,
+      bio
+    },
+    views,
+    description,
+    category,
+    image,
+    pitch
+  }
+}`);
